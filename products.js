d7160e8092ffaf45436bc4af0ef266e771ac86db
Edgar RP <jefelitman@gmail.com>
2023-02-16 21:48:01 -0500

    products.js v1.0.1

--- products.js
+++ products.js
@@ -1,18 +1,12 @@
 /* 
 This file contains all the code to generate and create the elements of products inside the page
 Made by: Edgar RP (JefeLitman) & Lina Ruiz 
-Version: 1.0.0
+Version: 1.0.1
 */
 
 create_product_card = (brand, product_name, disposables, area, subareas, tests) => {
     let parent_card = document.createElement("div");
     parent_card.classList.add("card");
-
-    let img = document.createElement("img");
-    img.src = "/gray_img.png";
-    img.classList.add("card-img-top", "d-none", "d-lg-flex");
-    img.style.maxHeight = "20vh";
-    parent_card.appendChild(img);
 
     let card_body = document.createElement("div");
     card_body.classList.add("card-body");
@@ -32,7 +26,7 @@ create_product_card = (brand, product_name, disposables, area, subareas, tests) 
     badges = badges.concat(subareas.slice(0,3), tests.slice(0,3));
     for (let idx in badges){
         let categories = document.createElement("span");
-        categories.classList.add("badge", "rounded-pill");
+        categories.classList.add("badge", "rounded-pill", "text-wrap", "m-1");
         if (idx == 0){
             categories.classList.add("text-bg-success");
         }
@@ -48,21 +42,45 @@ create_product_card = (brand, product_name, disposables, area, subareas, tests) 
     return parent_card;
 }
 
-display_products = (products) => {
+display_products = (products, id) => {
     // Always require a div container with the id 'products'
-    let product_div = document.getElementById("products");
+    let product_div = document.getElementById(id);
     for (let product of products){
         let responsive_div = document.createElement("div");
         responsive_div.classList.add("col-10", "col-lg-3", "my-3");
-        responsive_div.appendChild(create_product_card(
-            product.company,
-            product.name,
-            product.disposables,
-            product.area,
-            product.subareas,
-            product.tests
-        ));
-        product_div.appendChild(responsive_div);
+        if (id == "all"){
+            responsive_div.appendChild(create_product_card(
+                product.company,
+                product.name,
+                product.disposables,
+                product.area,
+                product.subareas,
+                product.tests
+            ));
+            product_div.appendChild(responsive_div);
+        }
+        if (product.area == "Point of Care" && id == "pointofcare"){
+            responsive_div.appendChild(create_product_card(
+                product.company,
+                product.name,
+                product.disposables,
+                product.area,
+                product.subareas,
+                product.tests
+            ));
+            product_div.appendChild(responsive_div);
+        }
+        if (product.area == "Labor" && id == "labor"){
+            responsive_div.appendChild(create_product_card(
+                product.company,
+                product.name,
+                product.disposables,
+                product.area,
+                product.subareas,
+                product.tests
+            ));
+            product_div.appendChild(responsive_div);
+        }
     }
     return true;
 }

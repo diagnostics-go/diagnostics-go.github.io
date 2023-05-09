4a171f889da1ed89f12c63ae6d1cdf71893115c6
Edgar RP <jefelitman@gmail.com>
2023-05-09 10:57:34 -0500

    JS Search v1.1

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
 /* 
 This file contains the logic to use the fuse.js search engine
 Made by: Edgar RP (JefeLitman) & Lina Ruiz 
-Version: 1.0
+Version: 1.1
 */
 
 get_search_engine = (products) => {
@@ -20,12 +20,30 @@ get_search_engine = (products) => {
       // ignoreFieldNorm: false,
       // fieldNormWeight: 1,
       keys: [
-        "company",
-        "name",
-        "disposables",
-        "area",
-        "subareas",
-        "tests"
+        {
+            name: 'company',
+            weight: 1
+        },
+        {
+            name: 'name',
+            weight: 2
+        },
+        {
+            name: 'disposables',
+            weight: 4
+        },
+        {
+            name: 'area',
+            weight: 8
+        },
+        {
+            name: 'subareas',
+            weight: 16
+        },
+        {
+            name: 'tests',
+            weight: 32
+        }
       ]
     };
     let fuse_object;
@@ -38,15 +56,43 @@ get_search_engine = (products) => {
 }
 
 search_result = (search_engine, text) => {
+    let text_lower = text.toLowerCase()
     let results = search_engine.search(text);
     let sorted_results = results.sort((a, b) => {
         if (a.score < b.score){
-            return -1
-        }
-        if (a.score > b.score){
             return 1
         }
+        if (a.score > b.score){
+            return -1
+        }
         return 0
+    }).map( r => r.item)
+    filtered_results = sorted_results.filter((value) => {
+        if (value.area.toLowerCase().includes(text_lower)){
+            return true;
+        }
+        if (value.company.toLowerCase().includes(text_lower)){
+            return true;
+        }
+        if (value.disposables.toLowerCase().includes(text_lower)){
+            return true;
+        }
+        if (value.name.toLowerCase().includes(text_lower)){
+            return true;
+        }
+        for (let element of value.subareas){
+            if (element.toLowerCase().includes(text_lower)){
+
+                return true;
+            }
+        }
+        for (let element of value.tests){
+            if (element.toLowerCase().includes(text_lower)){
+
+                return true;
+            }
+        }
+        return false;
     })
-    return sorted_results.map( r => r.item)
+    return filtered_results
 }
